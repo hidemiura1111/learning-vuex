0304a46c04d67e542de9218d8889484d958989be
hidems <miura.eleven@gmail.com>
2023-07-28 22:42:09 +0200

    Vuex actions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,11 +12,22 @@ export default {
   props: {
   },
   methods: {
+    // Call mutations
+    // increment() {
+    //   this.$store.commit('increment')
+    // },
+    // addCount() {
+    //   this.$store.commit('addCount', {
+    //     value: 10
+    //   })
+    // }
+
+    // Call actions
     increment() {
-      this.$store.commit('increment')
+      this.$store.dispatch('incrementAction')
     },
     addCount() {
-      this.$store.commit('addCount', {
+      this.$store.dispatch('addCountAction', {
         value: 10
       })
     }
